f9095f0b14dc5cff023ba297960ec6e75e526bd7
VictorZapata0612 <trolero304@gmail.com>
2022-06-02 11:16:20 -0500

    Actualizacion de iconos

--- correo_admin.php
+++ correo_admin.php
@@ -14,7 +14,7 @@
     <meta property="og:url" content="http://pratikborsadiya.in/blog/vali-admin">
     <meta property="og:image" content="http://pratikborsadiya.in/blog/vali-admin/hero-social.png">
     <meta property="og:description" content="Vali is a responsive and free admin theme built with Bootstrap 4, SASS and PUG.js. It's fully customizable and modular.">
-    <title>Docente</title>
+    <title>Admin</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -34,7 +34,7 @@
             <!-- User Menu-->
             <li class="dropdown"><a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
                 <ul class="dropdown-menu settings-menu dropdown-menu-right">
-                    <li><a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i>Inicio</a></li>
+                <li><a class="dropdown-item" href="home.php"><i class="fa fa-user fa-lg"></i>Inicio</a></li>
                 </ul>
             </li>
         </ul>
@@ -49,14 +49,15 @@
             </div>
         </div>
         <ul class="app-menu">
-        <li><a class="app-menu__item" href="home.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Inicio</span></a></li>
-            <li><a class="app-menu__item" href="registro.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Crear usuarios</span></a></li>
-            <li><a class="app-menu__item" href="estudiantes_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Estudiantes</span></a></li>
-            <li><a class="app-menu__item" href="correo_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Correos</span></a></li>
-            <li><a class="app-menu__item" href="Profesores_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Docentes</span></a></li>
-            <li><a class="app-menu__item" href=""><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Reportes</span></a></li>
-            <li><a class="app-menu__item" target="_blank" href="https://meet.google.com/kii-hgto-tmk"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Reuniones</span></a></li>
-            <li><a class="app-menu__item" href="calendario_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Calendario</span></a></li>
+            <li><a class="app-menu__item" href="home.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Inicio</span></a></li>
+            <li><a class="app-menu__item" href="registro.php"><i class="fa-solid fa-user-plus"></i><span class="app-menu__label">Crear usuarios</span></a></li>
+            <li><a class="app-menu__item" href="estudiantes_admin.php"><i class="fa-solid fa-child"></i><span class="app-menu__label">Estudiantes</span></a></li>
+            <li><a class="app-menu__item" href="correo_admin.php"><i class="fa-solid fa-envelope"></i><span class="app-menu__label">Correos</span></a></li>
+            <li><a class="app-menu__item" href="Profesores_admin.php"><i class="fa-solid fa-chalkboard-user"></i><span class="app-menu__label">Docentes</span></a></li>
+            <li><a class="app-menu__item" href=""><i class="fa-solid fa-exclamation"></i><span class="app-menu__label">Reportes</span></a></li>
+            <li><a class="app-menu__item" target="_blank" href="https://meet.google.com/kii-hgto-tmk"><i class="fa-solid fa-phone"></i><span class="app-menu__label">Reuniones</span></a></li>
+            <li><a class="app-menu__item" href="calendario_admin.php"><i class="fa-solid fa-calendar-days"></i><span class="app-menu__label">Calendario</span></a></li>
+            <li><a class="app-menu__item" href="index.php"><i class="fa-solid fa-right-from-bracket"></i><span class="app-menu__label">Cerrar sesión</span></a></li>
         </ul>
     </aside>
     <main class="app-content">
--- estudiantes_admin.php
+++ estudiantes_admin.php
@@ -24,12 +24,12 @@
     <!-- Font-icon css-->
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
-
+    <script src="https://kit.fontawesome.com/7ecad0e979.js" crossorigin="anonymous"></script>
 </head>
 
 <body class="app sidebar-mini">
     <!-- Navbar-->
-    <header class="app-header"><a class="app-header__logo" href="index.html">Admin</a>
+    <li><a class="dropdown-item" href="home.php"><i class="fa fa-user fa-lg"></i>Inicio</a></li>
         <!-- Sidebar toggle button--><a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
         <!-- Navbar Right Menu-->
         <ul class="app-nav">
@@ -51,14 +51,15 @@
             </div>
         </div>
         <ul class="app-menu">
-        <li><a class="app-menu__item" href="home.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Inicio</span></a></li>
-            <li><a class="app-menu__item" href="registro.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Crear usuarios</span></a></li>
-            <li><a class="app-menu__item" href="estudiantes_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Estudiantes</span></a></li>
-            <li><a class="app-menu__item" href="correo_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Correos</span></a></li>
-            <li><a class="app-menu__item" href="Profesores_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Docentes</span></a></li>
-            <li><a class="app-menu__item" href=""><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Reportes</span></a></li>
-            <li><a class="app-menu__item" target="_blank" href="https://meet.google.com/kii-hgto-tmk"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Reuniones</span></a></li>
-            <li><a class="app-menu__item" href="calendario_admin.php"><i class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Calendario</span></a></li>
+            <li><a class="app-menu__item" href="home.php"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Inicio</span></a></li>
+            <li><a class="app-menu__item" href="registro.php"><i class="fa-solid fa-user-plus"></i><span class="app-menu__label">Crear usuarios</span></a></li>
+            <li><a class="app-menu__item" href="estudiantes_admin.php"><i class="fa-solid fa-child"></i><span class="app-menu__label">Estudiantes</span></a></li>
+            <li><a class="app-menu__item" href="correo_admin.php"><i class="fa-solid fa-envelope"></i><span class="app-menu__label">Correos</span></a></li>
+            <li><a class="app-menu__item" href="Profesores_admin.php"><i class="fa-solid fa-chalkboard-user"></i><span class="app-menu__label">Docentes</span></a></li>
+            <li><a class="app-menu__item" href=""><i class="fa-solid fa-exclamation"></i><span class="app-menu__label">Reportes</span></a></li>
+            <li><a class="app-menu__item" target="_blank" href="https://meet.google.com/kii-hgto-tmk"><i class="fa-solid fa-phone"></i><span class="app-menu__label">Reuniones</span></a></li>
+            <li><a class="app-menu__item" href="calendario_admin.php"><i class="fa-solid fa-calendar-days"></i><span class="app-menu__label">Calendario</span></a></li>
+            <li><a class="app-menu__item" href="index.php"><i class="fa-solid fa-right-from-bracket"></i><span class="app-menu__label">Cerrar sesión</span></a></li>
         </ul>
     </aside>
     <main class="app-content">
